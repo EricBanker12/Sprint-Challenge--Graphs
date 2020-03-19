cbfe98699c812f3973be4d26e79b133730632983
ericbanker12 <ericbanker12@gmail.com>
2020-03-19 15:36:03 -0500

    add get_directions for directions from path, WIP add get_shortest_path

--- adv.py
+++ adv.py
@@ -2,6 +2,8 @@ from room import Room
 from player import Player
 from world import World
 
+import multiprocessing
+import os
 import random
 from ast import literal_eval
 
@@ -12,9 +14,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +31,59 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+def get_directions(path):
+    """return list of directions for given path"""
+    directions = []
 
+    for i in range(len(path) - 1):
+        room = world.rooms[path[i]]
+        next_room = world.rooms[path[i + 1]]
+
+        for direction in room.get_exits():
+            if next_room == room.get_room_in_direction(direction):
+                directions.append(direction)
+
+    return directions
+
+def get_path_always_right():
+    """return path for always going right, backtrack at dead ends or already visited"""
+    pass
+
+def get_shortest_path():
+    """return shortest path to visit all rooms"""
+    
+    def path_finder(queue):
+        try:
+            visited, path, room = queue.get(True, 1)
+        except multiprocessing.TimeoutError:
+            return
+
+        visited = visited.copy().add(room.id)
+        path = path.copy().append(room.id)
+
+        dead_end = True
+        for direction in 'nesw':
+            next_room = room.get_room_in_direction(direction)
+            if next_room:
+                if not next_room.id in visited:
+                    queue.put((visited, path, next_room))
+                    dead_end = False
+        
+        if dead_end:
+            get_shortest_unvisited_path()
+
+    queue = multiprocessing.Queue()
+    jobs = []
+    for i in range(1, os.cpu_count):
+        p = multiprocessing.Process(target=path_finder, args=(queue,))
+        jobs.append(p)
+
+    for p in jobs:
+        p.join()
+
+def get_shortest_unvisited_path(visited, path, room):
+    """return shortest path to a room adjacent to unvisited rooms"""
+    pass
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +105,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
